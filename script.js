965f3e67dff6674af0c32f672230e1d2838e5571
Amadeja <obsteter.amadeja@gmail.com>
2024-01-10 14:02:58 +0100

    Fix the behaviour of a prompt upon invalid value
    When a user enters invalid value, he will be prompted again until a
    valid value is entered, after which, the appropriate grid will be
    created.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ generateSquares();
 // on prompt, before anything else, take numOfSquares and remove each div, then prompt, which will change numofsquares and use the new numofsquares to uopdate the grid!
 
 function generateSquares() {
-  const squareWidth = 960 / numOfSquares;
+  const squareWidth = Math.floor(960 / numOfSquares);
   for (let i = 0; i < (numOfSquares * numOfSquares); i++) {
     // create and add div to the page, store in array
     squaresArray[i] = document.createElement('div');
@@ -19,10 +19,12 @@ function generateSquares() {
 }
 
 function removeSquares() {
+  if (squaresArray.length > 0) {
     for (let i = 0; i < (numOfSquares * numOfSquares); i++) {
-    board.removeChild(squaresArray[i]);
+      board.removeChild(squaresArray[i]);
   }
   clearArray();
+  }
 }
 
 function clearArray() {
@@ -38,6 +40,10 @@ function promptUser() {
   const userChoiceSquares = parseInt(prompt('Type in the number of squares you wish to generate (1 - 100)'));
   if (userChoiceSquares > 0 && userChoiceSquares <= 100) {
     numOfSquares = userChoiceSquares;
+    generateSquares();
+  } else {
+    // alert("Invalid input. Please enter an integer from 1 to 100!");
+    promptUser();
   }
 }
 // generate enough divs to fill the space
